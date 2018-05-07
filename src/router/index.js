6a1ed71a3956271157ca9da571270563dcba0837
seungkwon <yevgnenll@gmail.com>
2018-05-07 14:02:56 +0900

    change component name

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Satoshi from '@/components/Satoshi'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Satoshi',
+      component: Satoshi
     }
   ]
 })
